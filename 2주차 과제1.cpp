a07dd9788794dbc7e55d3abfd094dc84a66c5123
jangsw920 <jangsw920@gmail.com>
2026-09-15 18:39:31 +0900

    Add files via upload

---
+++ 2주차 과제1.cpp
@@ -0,0 +1,11 @@
+#include<stdio.h>
+
+int main(void) {
+	int val = 21;
+	int val minus = -21;
+		int result = 0;
+
+		result = val + val minus;
+		printf("result is %d\n", result);
+		return 0;
+}
---
+++ 2주차 과제2.cpp
@@ -0,0 +1,8 @@
+#include<stdio.h>
+
+int main(void) {
+	float num = 85.125;
+	double dnum = 85.125;
+
+	return 0;
+}
